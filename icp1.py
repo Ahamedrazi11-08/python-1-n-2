b3b208c8143fb4fb7f2f9f3c6c485ddc912c4e05
Ahamedrazi11-08 <47211061+Ahamedrazi11-08@users.noreply.github.com>
2019-06-04 13:05:30 -0500

    Add files via upload

---
+++ icp1.py
@@ -0,0 +1,24 @@
+from pip._vendor.distlib.compat import raw_input
+#takes the input
+inputstr = input ("Enter the String: ")
+
+
+
+
+#replacing characters
+print(inputstr.replace('py','java'))
+
+#reversing
+print(inputstr[::-1])
+
+
+#arithmetic operations
+a=1;
+b=2;
+c=a+b;
+print(c);
+
+d=3;
+e=2;
+f=d/e;
+print(f);
